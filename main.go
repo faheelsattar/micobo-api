275f576ca1413096735338193c2f7401cd37423b
faheelsattar <faheelsattar@yahoo.com>
2022-06-21 15:50:33 +0500

    feat: post route for adding employees

--- main.go
+++ main.go
@@ -19,14 +19,36 @@ var employees = []employee{
 	{ID: "2", Name: "James", Post: "Solidity Developer"},
 }
 
+func employeeSanitization(emp employee) bool {
+	return len(emp.ID) > 0 && len(emp.Name) > 0 && len(emp.Post) > 0
+}
+
 // getEmployees responds with the list of all employees as JSON.
 func getEmployees(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, employees)
 }
 
+func addEmployees(c *gin.Context) {
+	var newEmployee employee
+
+	if err := c.BindJSON(&newEmployee); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, "body is invalid")
+	}
+
+	if !employeeSanitization(newEmployee) {
+		c.IndentedJSON(http.StatusBadRequest, "body is invalid")
+	}
+
+	employees = append(employees, newEmployee)
+	c.IndentedJSON(http.StatusCreated, newEmployee)
+}
+
 func main() {
 	router := gin.Default()
+	//api routes
 	router.GET("/employees", getEmployees)
+	router.POST("/employess", addEmployees)
 
+	//server running
 	router.Run("localhost:8080")
 }

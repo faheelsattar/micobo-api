4b9935ab393d81b2be2402ff341f6f101f55763a
faheelsattar <faheelsattar@yahoo.com>
2022-06-22 14:00:37 +0500

    add: database config added

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module misobo
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.8.1
+require (
+	github.com/gin-gonic/gin v1.8.1
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,36 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+//database config
+var (
+	host     = ""
+	port     = 5432
+	user     = ""
+	password = ""
+	dbname   = ""
+)
+
+func databaseConnection() *sql.DB {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
 
 // employees data representation
 type employee struct {
@@ -25,6 +51,25 @@ func employeeSanitization(emp employee) bool {
 
 // getEmployees responds with the list of all employees as JSON.
 func getEmployees(c *gin.Context) {
+	db, ok := c.MustGet("db").(*sql.DB)
+	if !ok {
+		c.IndentedJSON(http.StatusBadGateway, "db connection not found")
+	}
+	rows, err := db.Query(`SELECT "Name", "Roll" FROM "Students"`)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var name string
+		var roll int
+
+		err = rows.Scan(&name, &roll)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(name, roll)
+	}
 	c.IndentedJSON(http.StatusOK, employees)
 }
 
@@ -43,9 +88,41 @@ func addEmployees(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newEmployee)
 }
 
+// ApiMiddleware will add the db connection to the context
+func DatabaseMiddleware(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("db", db)
+		c.Next()
+	}
+}
+
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
 func main() {
+	host = goDotEnvVariable("HOST")
+	i, err := strconv.Atoi((goDotEnvVariable("PORT")))
+	if err != nil {
+		fmt.Println(err)
+	}
+	port = i
+	user = goDotEnvVariable("USER")
+	password = goDotEnvVariable("PASSWORD")
+	dbname = goDotEnvVariable("DBNAME")
+
+	db := databaseConnection()
 	router := gin.Default()
 	//api routes
+	router.Use(DatabaseMiddleware(db))
 	router.GET("/employees", getEmployees)
 	router.POST("/employess", addEmployees)
 

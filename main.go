32570823404184adb56838fa78048b5c7a023101
faheelsattar <faheelsattar@yahoo.com>
2022-06-23 20:45:08 +0500

    add: event router added for attending employee

--- main.go
+++ main.go
@@ -13,13 +13,17 @@ func main() {
 	router := gin.Default()
 
 	//api routes
+
+	//employee routes
 	router.GET("/employees", controller.GetEmployees)
 	router.POST("/employees", controller.AddEmployees)
 	router.PUT("/employees/:employee_id", controller.UpdateEmployee)
 	router.DELETE("/employees/:employee_id", controller.DeleteEmployee)
 
+	//event routes
 	router.GET("/events", controller.GetEvents)
-	router.GET("/events/event_id", controller.GetEvent)
+	router.GET("/events/:event_id", controller.GetEvent)
+	router.GET("/events/:event_id/employees", controller.GetEmployeesAttendingEvent)
 
 	//server running
 	router.Run("localhost:8080")
